3605c271d414b0f60fda0066b63cfdb56be055ce
Ronak Dutta <ronakdutta100@gmail.com>
2026-05-02 20:30:18 +0530

    Fixed issues

--- server/index.js
+++ server/index.js
@@ -9,7 +9,7 @@ import roleRoutes from "./routes/roleRoutes.js";
 import { login, signup } from "./controller/authController.js";
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json());
