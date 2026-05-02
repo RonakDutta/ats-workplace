a273981a7f40663a1b16c3367f677eebb7c14cb9
Ronak Dutta <ronakdutta100@gmail.com>
2026-05-02 20:23:54 +0530

    Fixed backend connection

--- client/src/services/api.js
+++ client/src/services/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL =
-  import.meta.env.VITE_API_URL || "http://localhost:5000/api";
+const API_BASE_URL = "https://ats-workplace-backend.onrender.com/api";
 
 axios.interceptors.request.use((config) => {
   const token = localStorage.getItem("ats_token");
